c8c9e5fe0da5bb2574ac9a95154ced6073525fe2
Dhirajrai2221 <113769910+Dhirajrai2221@users.noreply.github.com>
2023-10-28 19:23:11 +0545

    minor changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
         "cl-2": "#D3D3D3",
         "cl-3": "#A09F9F",
         "cl-4": "#E7E7E7",
+        "cl-5": "#444",
 
         mainBgcl: "#F2F5F8",
         lightBlue: "#6DACE7",
@@ -27,7 +28,7 @@ module.exports = {
         navShadow: " 0px 4px 26px 0px rgba(191, 190, 190, 0.25)",
         cardShadow: "5px 8px 20px 0px rgba(191, 191, 191, 0.25)",
         toogleBtnShadow: "0 5px 8px 20px 0px rgba(191, 191, 191, 0.25)",
-        leftRightShadow: '0px 16px 20px 0px rgba(80, 80, 80, 0.20)',
+        leftRightShadow: "0px 16px 20px 0px rgba(80, 80, 80, 0.20)",
       },
       gridTemplateColumns: {
         gridfluid: "repeat(auto-fit, minmax(10rem, 1fr))",
